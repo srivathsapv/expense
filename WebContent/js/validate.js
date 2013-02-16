4c13afc3e4b70176bfafa958593c36b7b73b8e35
Sasi Praveen <sasipraveen39@gmail.com>
2013-02-16 08:16:41 +0530

    fixes for #7

--- WebContent/js/validate.js
+++ WebContent/js/validate.js
@@ -83,11 +83,13 @@ function ajaxcheck(currentElement,type){
 	var element;
 	var valmsg = $(currentElement).attr('valmsg');		
 	if($(currentElement).attr('class').indexOf('prepend-input') >= 0 || $(currentElement).attr('class').indexOf('append-input') >= 0 || $(currentElement).attr('class').indexOf('required') >= 0)
-	{	
+	{		
 		element = $(currentElement).parent();
 		try{
 		if($(currentElement).parent().parent().attr('class').indexOf('input-prepend') >= 0)
 			element = $(currentElement).parent().parent();
+		if($(currentElement).parent().parent().parent().attr('class').indexOf('input-append') >= 0)
+			element = $(currentElement).parent().parent().parent();
 		}catch(err){
 			//do nothing
 		}
